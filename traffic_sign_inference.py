f69a07a5bee5e56d540e3a5ac5a05e8edd805b26
Marek Kolodziej <mkolod@gmail.com>
2017-03-18 01:26:01 +0000

    Solution to the traffic sign inference image size problem

--- traffic_sign_inference.py
+++ traffic_sign_inference.py
@@ -10,15 +10,13 @@ from scipy.misc import imread
 from caffe_classes import class_names
 from alexnet import AlexNet
 
+
+# placeholders
 x = tf.placeholder(tf.float32, (None, 32, 32, 3))
-# TODO: Resize the images so they can be fed into AlexNet.
-# HINT: Use `tf.image.resize_images` to resize the images
-resized = ...
+resized = tf.image.resize_images(x, (227, 227))
 
-assert resized is not Ellipsis, "resized needs to modify the placeholder image size to (227,227)"
 probs = AlexNet(resized)
-
-init = tf.global_variables_initializer()
+init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
